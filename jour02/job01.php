52f2edca2398205dfd988e5a84136d7b1a639952
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-02-10 10:55:13 +0100

    feat : job01

---
+++ jour02/job01.php
@@ -0,0 +1,38 @@
+<?php
+
+class Rectangle {
+    private int $longueur;
+    private int $largeur;
+
+    function __construct(int $longueur, int $largeur){
+        $this->longueur = $longueur;
+        $this->largeur = $largeur;
+    }
+
+    function getLongueur(): int{
+        return $this->longueur;
+    }
+
+    function getLargeur(): int{
+        return $this->largeur;
+    }
+
+    function setLongueur(int $newLongueur){
+        $this->longueur = $newLongueur;
+    }
+
+    function setLargeur(int $newLargeur){
+        $this->largeur = $newLargeur;
+    }
+}
+
+$rectangle = new Rectangle(10,5);
+echo("Longueur du rectangle : " . $rectangle->getLongueur() . "<br>");
+echo("Largeur du rectangle : " . $rectangle->getLargeur() . "<br>");
+echo($rectangle->setLongueur(8));
+echo($rectangle->setLargeur(3));
+echo("Nouvelle Longueur du rectangle : " . $rectangle->getLongueur() . "<br>");
+echo("Nouvelle Largeur du rectangle : " . $rectangle->getLargeur() . "<br>");
+
+
+?>
